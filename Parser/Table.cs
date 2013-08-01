9748e639a9f22812992b1ca87e397fa61cb994a1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-08-01 22:31:17 +0200

    Removed unused code

--- Parser/Table.cs
+++ Parser/Table.cs
@@ -62,17 +62,6 @@ namespace CanvasDiagramEditor.Parser
         public string ArchiveNo { get; set; }
 
         #endregion
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public void Notify(string propertyName)
-        {
-            var handler = PropertyChanged;
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
     }
 
     #endregion
