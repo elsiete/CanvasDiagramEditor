c6393d1299433f246c69dd8f71000bb661a483c2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-08-22 17:33:19 +0200

    Added 4 item Tuple

--- CanvasDiagram.Util/Tuple.cs
+++ CanvasDiagram.Util/Tuple.cs
@@ -50,5 +50,21 @@ namespace CanvasDiagram.Util
         }
     }
 
+    public class Tuple<T1, T2, T3, T4>
+    {
+        public T1 Item1 { get; private set; }
+        public T2 Item2 { get; private set; }
+        public T3 Item3 { get; private set; }
+        public T4 Item4 { get; private set; }
+
+        public Tuple(T1 item1, T2 item2, T3 item3, T4 item4)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+            Item4 = item4;
+        }
+    }
+
     #endregion
 }
