6ff1c48f1a2f668301c86d962fb4dc39e575f714
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-09-28 22:38:39 +0200

    Switched to Aliases class representation

--- CanvasDiagram.Core/IDiagramParser.cs
+++ CanvasDiagram.Core/IDiagramParser.cs
@@ -15,21 +15,21 @@ namespace CanvasDiagram.Core
 {
     #region Aliases
 
-    using MapPin = Tuple<string, string>;
-    using MapWire = Tuple<object, object, object>;
-    using MapWires = Tuple<object, List<Tuple<string, string>>>;
-    using Selection = Tuple<bool, List<Tuple<object, object, object>>>;
-    using UndoRedo = Tuple<Stack<string>, Stack<string>>;
-    using Diagram = Tuple<string, Tuple<Stack<string>, Stack<string>>>;
-    using TreeDiagram = Stack<string>;
-    using TreeDiagrams = Stack<Stack<string>>;
-    using TreeProject = Tuple<string, Stack<Stack<string>>>;
-    using TreeProjects = Stack<Tuple<string, Stack<Stack<string>>>>;
-    using TreeSolution = Tuple<string, string, string, Stack<Tuple<string, Stack<Stack<string>>>>>;
-    using Position = Tuple<double, double>;
-    using Connection = Tuple<IElement, List<Tuple<object, object, object>>>;
-    using Connections = List<Tuple<IElement, List<Tuple<object, object, object>>>>;
-    using Solution = Tuple<string, IEnumerable<string>>;
+    //using MapPin = Tuple<string, string>;
+    //using MapWire = Tuple<object, object, object>;
+    //using MapWires = Tuple<object, List<Tuple<string, string>>>;
+    //using Selection = Tuple<bool, List<Tuple<object, object, object>>>;
+    //using UndoRedo = Tuple<Stack<string>, Stack<string>>;
+    //using Diagram = Tuple<string, Tuple<Stack<string>, Stack<string>>>;
+    //using TreeDiagram = Stack<string>;
+    //using TreeDiagrams = Stack<Stack<string>>;
+    //using TreeProject = Tuple<string, Stack<Stack<string>>>;
+    //using TreeProjects = Stack<Tuple<string, Stack<Stack<string>>>>;
+    //using TreeSolution = Tuple<string, string, string, Stack<Tuple<string, Stack<Stack<string>>>>>;
+    //using Position = Tuple<double, double>;
+    //using Connection = Tuple<IElement, List<Tuple<object, object, object>>>;
+    //using Connections = List<Tuple<IElement, List<Tuple<object, object, object>>>>;
+    //using Solution = Tuple<string, IEnumerable<string>>;
 
     #endregion
 
