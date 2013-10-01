76cbcb99fb11fa936921c5f8a69c504452ee784b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2013-10-01 16:58:00 +0200

    Updated Build script

--- Build-Debug-v3.5.cmd
+++ Build-Debug-v3.5.cmd
@@ -1,6 +1,6 @@
 @echo off
 set PATH=C:\WINDOWS\Microsoft.NET\Framework\v3.5;%PATH%
 
-MSBuild CanvasDiagramEditor.sln /m /t:Rebuild /p:Configuration=Debug;TargetFrameworkVersion=v3.5
+MSBuild CanvasDiagramEditor.sln /m /t:Build /p:Configuration=Debug;TargetFrameworkVersion=v3.5
 
 pause
--- Build-Release-v3.5.cmd
+++ Build-Release-v3.5.cmd
@@ -1,6 +1,6 @@
 @echo off
 set PATH=C:\WINDOWS\Microsoft.NET\Framework\v3.5;%PATH%
 
-MSBuild CanvasDiagramEditor.sln /m /t:Rebuild /p:Configuration=Release;TargetFrameworkVersion=v3.5
+MSBuild CanvasDiagramEditor.sln /m /t:Build /p:Configuration=Release;TargetFrameworkVersion=v3.5
 
 pause
