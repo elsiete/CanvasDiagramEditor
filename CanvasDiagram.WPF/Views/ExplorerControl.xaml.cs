21eacdf0dcb2537b2c83219e1327aca47db51985
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-03-25 19:10:17 +0100

    WPF license headers

--- CanvasDiagram.WPF/Views/ExplorerControl.xaml.cs
+++ CanvasDiagram.WPF/Views/ExplorerControl.xaml.cs
@@ -1,6 +1,5 @@
-﻿// Copyright (C) Wiesław Šoltés 2013. 
-// All Rights Reserved
-
+﻿// Copyright (c) Wiesław Šoltés. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 #region References
 
 using CanvasDiagram.WPF.Controls;
