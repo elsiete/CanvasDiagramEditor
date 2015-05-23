8437f9d4556eb1ba9262c6800425c670f7f38d6b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2015-05-23 23:24:46 +0200

    Refactored solution

--- Build-Debug-v3.5.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set PATH=C:\WINDOWS\Microsoft.NET\Framework\v3.5;%PATH%
-
-MSBuild CanvasDiagramEditor.sln /m /t:Build /p:Configuration=Debug;TargetFrameworkVersion=v3.5
-
-pause
--- Build-Release-v3.5.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set PATH=C:\WINDOWS\Microsoft.NET\Framework\v3.5;%PATH%
-
-MSBuild CanvasDiagramEditor.sln /m /t:Build /p:Configuration=Release;TargetFrameworkVersion=v3.5
-
-pause
